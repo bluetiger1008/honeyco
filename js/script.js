507759f81ef1a406c91a481e1d040decf9f2e484
marcinrobak1008 <marcin@Administrators-PC-4.local>
2016-10-05 22:36:04 +0200

    fix issue

--- js/script.js
+++ js/script.js
@@ -12,32 +12,66 @@ $(document).ready(function() {
     }
   });
   $('.menu-link').bigSlide();
+  $( '.swipebox-video' ).click( function( e ) {
+    e.preventDefault();
+    $.swipebox( [
+      { href:'http://vimeo.com/29193046', title:'My Caption', rel:'vimeo' }
+    ] );
+  } );
 });
+
 $('.carousel').carousel({
     interval: false
 });
-$('.btn-yellow').click(function() {
+
+$('.btn-learn').click(function() {
     $('html, body').animate({
         scrollTop: $('.connect').offset().top
     }, 1000);
 });
+
+var close_setting = {
+  'state': 'closed'
+};
+
+$('.nav-join').click(function() {
+    $('html, body').animate({
+        scrollTop: $('.connect').offset().top
+    }, 1000);
+    
+    $('.menu-link').bigSlide(close_setting);
+});
+
+$('.btn-discover').click(function() {
+    $('html, body').animate({
+        scrollTop: $('.connect').offset().top
+    }, 1000);
+});
+
 $('.nav-about').click(function() {
     $('html, body').animate({
         scrollTop: $('.retirement').offset().top
     }, 1000);
+    $('.menu-link').bigSlide(close_setting);
 });
+
 $('.nav-home').click(function() {
     $('html, body').animate({
         scrollTop: $('.security').offset().top
     }, 1000);
+    $('.menu-link').bigSlide(close_setting);
 });
+
 $('.nav-tech').click(function() {
     $('html, body').animate({
         scrollTop: $('.gallery').offset().top
     }, 1000);
+    $('.menu-link').bigSlide(close_setting);
 });
+
 $('.nav-community').click(function() {
     $('html, body').animate({
         scrollTop: $('.community').offset().top
     }, 1000);
+    $('.menu-link').bigSlide(close_setting);
 });
